8420076027426204a2d433b93dbb7baa3408ac5c
Sujay mallikarjun huralikoppi <sujaymh27@gmail.com>
2025-05-21 23:28:47 +0530

    Update triangle_continued.java
    added the another triangular pattern

--- triangle_continued.java
+++ triangle_continued.java
@@ -71,6 +71,40 @@ public class triangle_continued {
 //         @@@@@@@@@
 //        @@@@@@@@@@@
 
-
+       for (int i=1;i<=n;i++){
+        for (int j=n;j>=i;j--){
+            System.out.print("@");
+        }for (int k=2;k<=i;k++){
+            System.out.print(" ");
+        }for (int l=2;l<=i;l++){
+            System.out.print(" ");
+        }for (int m=n;m>=i;m--){
+            System.out.print("@");
+        }
+        System.out.println();
+    }
+    for (int i=2;i<=n;i++){
+        for (int j=1;j<=i;j++){
+            System.out.print("@");
+        }for (int k=n-1;k>=i;k--){
+            System.out.print(" ");
+        }for (int l=n-1;l>=i;l--){
+            System.out.print(" ");
+        }for (int m=1;m<=i;m++){
+            System.out.print("@");
+        }
+        System.out.println();
+    }
+//        @@@@@@@@@@@@
+//        @@@@@  @@@@@
+//        @@@@    @@@@
+//        @@@      @@@
+//        @@        @@
+//        @          @
+//        @@        @@
+//        @@@      @@@
+//        @@@@    @@@@
+//        @@@@@  @@@@@
+//        @@@@@@@@@@@@
     }
 }
